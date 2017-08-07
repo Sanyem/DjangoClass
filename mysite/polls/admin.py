f5fb375f4cf1a0c5205f616cb21ef4fa09e1dfb3
Sanyem <sanyem.g16@iiits.in>
2017-08-07 09:30:20 +0530

    Add files via upload

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import Question
+from .models import Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
 
 # Register your models here.
